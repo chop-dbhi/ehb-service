7481879365f1eb14815c8304de1d02ca0f72753b
Tyler Rivera <riverat2@email.chop.edu>
2016-10-13 14:32:55 -0400

    Test locally instead of in Docker container
    Export all environment variables during testing
    
    Include test settings in script
    
    Build docker image and push as deployment

--- test.sh
+++ test.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+export APP_ENV=test
+export DEBUG=1
+export SECRET_KEY="test"
+export DATABASE_URL=sqlite:///ehb_service.db
 
-DEBUG=1
-SECRET_KEY="test"
-DATABASE_URL=sqlite:///ehb_service.db
+export EMAIL_HOST=localhost
+export EMAIL_PORT=25
+export EMAIL_DOMAIN=example.com
 
-EMAIL_HOST=localhost
-EMAIL_PORT=25
-EMAIL_DOMAIN=example.com
+export EHB_USE_ENCRYPTION=1
+export EHB_KMS_SECRET=ZAXxDHJAqyMfVR3sLjydxjjhgkgyZM7J
+export EHB_CLIENT_KEY_SEED=782399698
+export EHB_GROUP_KEY_SEED=477397326
 
-EHB_USE_ENCRYPTION=0
-EHB_KMS_SECRET="secretsecret"
-EHB_CLIENT_KEY_SEED=1
-EHB_GROUP_KEY_SEED=1
+export LDAP_DEBUG=0
+export LDAP_PREBINDDN=
+export LDAP_SEARCHDN=
+export LDAP_SEARCH_FILTER=
+export LDAP_SERVER_URI=
+export LDAP_PREBINDPW=
 
 ./bin/manage.py syncdb --noinput
 ./bin/manage.py migrate --noinput
