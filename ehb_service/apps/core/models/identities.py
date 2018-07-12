69e8d49cd4956eb87cfac26e7f0375daae85f75e
szh2425 <szh2425@gmail.com>
2018-07-12 10:49:10 -0400

    fixing identities file

--- ehb_service/apps/core/models/identities.py
+++ ehb_service/apps/core/models/identities.py
@@ -424,10 +424,14 @@ class ExternalRecordRelation(CreatedModified):
         }
 
     def __unicode__(self):
-        return "{0} related to {1}, -- Type: {2}".format(
-              self.external_record,
-              self.related_record,
+        return "{0}, {1} ({5}) related to {2}, {3} ({6}) -- Type: {4}".format(
+              self.external_record.subject.last_name,
+              self.external_record.subject.first_name,
+              self.related_record.subject.last_name,
+              self.related_record.subject.first_name,
               self.relation_type,
+              self.external_record.external_system.name,
+ -            self.related_record.external_system.name
               )
 
 
