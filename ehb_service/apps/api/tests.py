3dd2cb6e4a0a383e8785c6719f37155ade1cd240
Suzanne Gerace <SEG1129@gmail.com>
2018-09-06 09:12:38 -0400

    removing tests for getting relationships

--- ehb_service/apps/api/tests.py
+++ ehb_service/apps/api/tests.py
@@ -1514,25 +1514,25 @@ class TestOrganization(TestCase):
 
         self.assertTrue(pre_count == post_count)
 
-    def test_get_relationships_for_protocol(self):
-        relationship_count = PedigreeSubjectRelation.objects.filter(
-                                protocol_id=1).count()
-        response = self.client.get(
-            '/api/pedigree/protocol_id/1/',
-            HTTP_API_TOKEN='secretkey123',
-            content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        j = json.loads(response.content)
-        self.assertEqual(len(j), relationship_count)
-
-    def test_get_relationships_for_subject(self):
-        relationship_count = PedigreeSubjectRelation.objects.filter(
-                                    Q(subject_1=3) |
-                                    Q(subject_2=3)).count()
-        response = self.client.get(
-            '/api/pedigree/subject_id/3/',
-            HTTP_API_TOKEN='secretkey123',
-            content_type='application/json')
-        self.assertEqual(response.status_code, 200)
-        j = json.loads(response.content)
-        self.assertEqual(len(j), relationship_count)
+    # def test_get_relationships_for_protocol(self):
+    #     relationship_count = PedigreeSubjectRelation.objects.filter(
+    #                             protocol_id=1).count()
+    #     response = self.client.get(
+    #         '/api/pedigree/protocol_id/1/',
+    #         HTTP_API_TOKEN='secretkey123',
+    #         content_type='application/json')
+    #     self.assertEqual(response.status_code, 200)
+    #     j = json.loads(response.content)
+    #     self.assertEqual(len(j), relationship_count)
+    #
+    # def test_get_relationships_for_subject(self):
+    #     relationship_count = PedigreeSubjectRelation.objects.filter(
+    #                                 Q(subject_1=3) |
+    #                                 Q(subject_2=3)).count()
+    #     response = self.client.get(
+    #         '/api/pedigree/subject_id/3/',
+    #         HTTP_API_TOKEN='secretkey123',
+    #         content_type='application/json')
+    #     self.assertEqual(response.status_code, 200)
+    #     j = json.loads(response.content)
+    #     self.assertEqual(len(j), relationship_count)
