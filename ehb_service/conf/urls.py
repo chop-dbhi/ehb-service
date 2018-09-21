962ff6b37dfd2f74813e8d50c77aab7f4f23fe3e
szh2425 <szh2425@gmail.com>
2018-09-21 13:34:10 -0400

    redirectview requires permanent arguement to be set and urlpatterns is deprecated

--- ehb_service/conf/urls.py
+++ ehb_service/conf/urls.py
@@ -2,12 +2,12 @@ from django.conf.urls import *
 from django.contrib import admin
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic.base import RedirectView
+# from .apps.api import urls
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('api.urls')),
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'))),
-)
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('admin:index'), permanent=True)),
+]
