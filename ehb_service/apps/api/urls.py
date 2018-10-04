40823f18ef5d6be3f7df607ec24638d972139d4d
szh2425 <szh2425@gmail.com>
2018-10-04 10:39:06 -0400

    adding app_name for url patterns to address deprecation

--- ehb_service/apps/api/urls.py
+++ ehb_service/apps/api/urls.py
@@ -1,22 +1,23 @@
 from django.conf.urls import url, include  # noqa
 from api.resources import subject, relation, organization, group, externalsystem,externalrecord
 
-subject_patterns = [
+
+subject_patterns = ([
     # 'api.resources.subject',
     url(r'^$', subject.SubjectResource),
     url(r'^id/(?P<pk>\d+)/$', subject.SubjectResource),
     url(r'^organization/(?P<org_pk>\d+)/osid/(?P<osid>\w+)/$', subject.SubjectResource),
-    url(r'^externalrecsys/(?P<externalrecsys>\d+)/erid/(?P<erid>.*)/$', subject.SubjectResource),
-]
+    url(r'^externalrecsys/(?P<externalrecsys>\d+)/erid/(?P<erid>.*)/$', subject.SubjectResource),],
+    'api')
 
-organization_patterns = [
+organization_patterns = ([
     # 'api.resources.organization',
     url(r'^$', organization.OrganizationResource),
     url(r'^id/(?P<pk>\d+)/$', organization.OrganizationResource),
-    url(r'^query/$', organization.OrganizationQuery),
-]
+    url(r'^query/$', organization.OrganizationQuery),],
+    'api')
 
-externalSystem_patterns = [
+externalSystem_patterns = ([
     # 'api.resources.externalsystem',
     url(r'^$', externalsystem.ExternalSystemResource),
     url(r'^id/(?P<pk>\d+)/$', externalsystem.ExternalSystemResource),
@@ -24,10 +25,10 @@ externalSystem_patterns = [
     url(r'^id/(?P<pk>\d+)/organization/(?P<org_pk>\d+)/subjects/$', externalsystem.ExternalSystemSubjects),
     url(r'^id/(?P<pk>\d+)/records/$', externalsystem.ExternalSystemRecords),
     url(r'^id/(?P<pk>\d+)/organization/(?P<org_pk>\d+)/records/$', externalsystem.ExternalSystemRecords),
-    url(r'^query/$', externalsystem.ExternalSystemQuery),
-]
+    url(r'^query/$', externalsystem.ExternalSystemQuery),],
+    'api')
 
-externalRecord_patterns = [
+externalRecord_patterns = ([
     # 'api.resources.externalrecord',
     url(r'^$', externalrecord.ExternalRecordResource),
     url(r'^id/(?P<pk>\d+)/$', externalrecord.ExternalRecordResource),
@@ -35,25 +36,26 @@ externalRecord_patterns = [
     url(r'^id/(?P<pk>\d+)/links/(?P<link>\d+)/$', externalrecord.ExternalRecordRelationResource),
     url(r'^query/$', externalrecord.ExternalRecordQuery),
     url(r'^labels/$', externalrecord.ExternalRecordLabelResource),
-    url(r'^labels/(?P<pk>\d+)/$', externalrecord.ExternalRecordLabelResource),
-]
+    url(r'^labels/(?P<pk>\d+)/$', externalrecord.ExternalRecordLabelResource),],
+    'api')
 
-group_patterns = [
+
+group_patterns = ([
     # 'api.resources.group',
     url(r'^$', group.GroupResource),
     url(r'^id/(?P<pk>\d+)/subjects/$', group.SubjectGroupResource),
     url(r'^id/(?P<grp_pk>\d+)/subjects/id/(?P<x_pk>\d+)/$', group.SubjectGroupResource),
     url(r'^id/(?P<pk>\d+)/records/$', group.RecordGroupResource),
-    url(r'^id/(?P<grp_pk>\d+)/records/id/(?P<x_pk>\d+)/$', group.RecordGroupResource),
-]
+    url(r'^id/(?P<grp_pk>\d+)/records/id/(?P<x_pk>\d+)/$', group.RecordGroupResource),],
+    'api')
 
-pedigreeRelationship_patterns = [
+pedigreeRelationship_patterns = ([
     # 'api.resources.relation',
     url(r'^$', relation.PedigreeSubjectRelationResource),
     url(r'^protocol_id/(?P<protocol_id>\d+)/$', relation.PedigreeSubjectRelationResource),
-    url(r'^subject_id/(?P<subject_id>\d+)/$', relation.PedigreeSubjectRelationResource),
+    url(r'^subject_id/(?P<subject_id>\d+)/$', relation.PedigreeSubjectRelationResource),],
+    'api')
     #url(r'^organization/(?P<org_pk>\d+)/osid/(?P<osid>\w+)/$', 'PedigreeSubjectRelationResource'),
-]
 
 urlpatterns = [
     url(r'^subject/', include(subject_patterns,
