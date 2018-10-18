1dc9c74ad9f2dbea134f86fcec94516bb0902a76
szh2425 <szh2425@gmail.com>
2018-10-18 16:01:22 -0400

    adding parser and render settings for restframework

--- ehb_service/conf/settings.py
+++ ehb_service/conf/settings.py
@@ -75,6 +75,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    ),
     # 'DEFAULT_PERMISSION_CLASSES': [
     #     'rest_framework.permissions.IsAuthenticatedOrReadOnly'
     #     # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
