7672f0ca6295b70ce0d54692c06fba269f3f5d09
szh2425 <szh2425@gmail.com>
2018-10-26 10:34:46 -0400

    removing serializer

--- ehb_service/apps/api/views/externalsystem.py
+++ ehb_service/apps/api/views/externalsystem.py
@@ -6,8 +6,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import permission_classes
 from rest_framework import permissions
-from api.serializers import ExternalSystemSerializer
-from django.http import HttpResponse
 
 from api.helpers import FormHelpers
 from constants import ErrorConstants
--- ehb_service/apps/api/views/organization.py
+++ ehb_service/apps/api/views/organization.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import permission_classes
 from rest_framework import permissions
-from api.serializers import OrganizationSerializer
 
 from api.helpers import FormHelpers
 from constants import ErrorConstants
